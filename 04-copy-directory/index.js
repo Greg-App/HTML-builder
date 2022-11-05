2f20060ebc80358e5bf434b89030350f17df5aa8
Greg App <greg358@mail.ru>
2022-11-05 18:23:59 +0300

    refactor: change  some operational info to console

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,7 +4,7 @@ const fsProm = require('fs/promises');
 
 const sourceFolder = path.resolve(__dirname, 'files');
 const targetFolder = path.resolve(__dirname, 'files-copy');
-//fsProm.mkdir(targetFolder, {recursive:true});
+
 
 fsProm.readdir(sourceFolder, {
     withFileTypes: true
@@ -16,7 +16,6 @@ fsProm.readdir(sourceFolder, {
         fileList.push(file.name);
       }
     }
-    if (fileList.length > 0) {
       fsProm.mkdir(targetFolder, {
           recursive: true
         })
@@ -26,23 +25,24 @@ fsProm.readdir(sourceFolder, {
             if(oldTargetList.length>0) {
               for (let file of oldTargetList) {
                 if (!fileList.includes(file)) {
-                  console.log(file, 'file in file-copy folder need to be removed');
+                  console.log('file in files-copy folder has to be removed: ',file);
                   fs.rm(path.resolve(targetFolder,file),(err) => {
                     if(err){
                         // File deletion failed
                         console.error(err.message);
                         return;
                     }
-                    console.log(`file ${file} DELETING complete`);
+                    console.log(`REMOVE file complete from files-copy: ${file}`);
                 });
-                 
                 }
+                
               }
+              console.log('\n');
             }
             for (let i = 0; i < fileList.length; i++) {
               fsProm.copyFile(path.resolve(sourceFolder,fileList[i]),path.resolve(targetFolder,fileList[i]))
               .then(()=>{
-                console.log(`file ${fileList[i]} copying complete`);
+                console.log(`copy/update file complete from files to files-copy: ${fileList[i]}`);
             });
             }
           });
@@ -51,7 +51,7 @@ fsProm.readdir(sourceFolder, {
         .catch(err => {
           console.error(err);
         });
-    }
+   
   })
   .catch(err => {
     console.error(err);
