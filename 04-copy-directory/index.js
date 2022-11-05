3d4c55ce1b85d1f7986fac1ef7e3d29ae92816eb
Greg App <greg358@mail.ru>
2022-11-05 17:43:47 +0300

    feat: add functionality remove extra files from file-copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -21,12 +21,32 @@ fsProm.readdir(sourceFolder, {
           recursive: true
         })
         .then(() => {
-          for (let i = 0; i < fileList.length; i++) {
-            fsProm.copyFile(path.resolve(sourceFolder,fileList[i]),path.resolve(targetFolder,fileList[i]))
-            .then(()=>{
-              console.log(`file ${i} copying complete`);
+          fsProm.readdir(targetFolder)
+          .then((oldTargetList)=>{
+            if(oldTargetList.length>0) {
+              for (let file of oldTargetList) {
+                if (!fileList.includes(file)) {
+                  console.log(file, 'file in file-copy folder need to be removed');
+                  fs.rm(path.resolve(targetFolder,file),(err) => {
+                    if(err){
+                        // File deletion failed
+                        console.error(err.message);
+                        return;
+                    }
+                    console.log(`file ${file} DELETING complete`);
+                });
+                 
+                }
+              }
+            }
+            for (let i = 0; i < fileList.length; i++) {
+              fsProm.copyFile(path.resolve(sourceFolder,fileList[i]),path.resolve(targetFolder,fileList[i]))
+              .then(()=>{
+                console.log(`file ${fileList[i]} copying complete`);
+            });
+            }
           });
-          }
+
         })
         .catch(err => {
           console.error(err);
