190847b491f555a1b57597b47b6c78031a6b0391
Greg App <greg358@mail.ru>
2022-11-06 22:34:10 +0300

    feat: complete 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,32 +2,42 @@ const path = require('path');
 const fs = require('fs');
 const fsProm = require('fs/promises');
 
-async function getStylesArray(folder) {
-  const filesArr = await getCssFilesArr(folder).then(res => res);
-  console.log(filesArr, 'arr');
+async function getFileContentObjArray(folder,ext) {
+  const filesArr = await getFilesArr(folder,ext).then(res => res);
+  console.log('File list to bundle:',filesArr);
   const stylesArrProm = Promise.all(filesArr.map((elem) => {
-    console.log(path.resolve(folder,elem));
     return fsProm.readFile(path.resolve(folder,elem),'utf-8');
   }));
   const stylesArr=await stylesArrProm.then(res=>res);
   const styleObjArr=[];
   for (let i=0;i<filesArr.length;i++) {
-    //styleObjArr.push({[filesArr[i].slice(0,filesArr[i].length-4)]:stylesArr[i]});
     styleObjArr.push({[filesArr[i].slice(0,filesArr[i].lastIndexOf("."))]:stylesArr[i]});
   }
   return styleObjArr;
 }
-async function getCssFilesArr(folder) {
+async function getFilesArr(folder, ext) {
   const fileList = await fsProm.readdir(folder, {
     withFileTypes: true
   }).then(res => res);
   const fileArr = fileList.filter((file) => {
-    return (file.isDirectory() === false) && (path.extname(path.resolve(folder, file.name)) === '.css');
+    return (file.isDirectory() === false) && (path.extname(path.resolve(folder, file.name)) === `.${ext}`);
   }).map((e) => e.name);
   return fileArr;
 }
-//getCssFilesArr (path.resolve(__dirname,'styles')).then(re=>console.log(re,'result'));
-getStylesArray(path.resolve(__dirname, 'styles')).then(re => console.log(re, 'result'));
+
+async function appendFileContent(trgtFilePath,dataArr) {
+  const fileContent=dataArr
+  .map((elem)=>Object.values(elem)[0])
+  .join('\n');
+  console.log('\nNew bundle.css file:\n-------------------\n',fileContent);
+  return fsProm.writeFile(trgtFilePath,fileContent);
+}
+
+async function bundleCssFiles () {
+  const fileArr=await getFileContentObjArray(path.resolve(__dirname, 'styles'),'css');
+  await appendFileContent(path.resolve(__dirname, 'project-dist','bundle.css'),fileArr);
+}
+bundleCssFiles ().then(_res=>console.log('____________________\n','Bundle files done'));
 
 
 
