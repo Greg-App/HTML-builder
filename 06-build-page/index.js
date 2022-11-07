0e7dca0dc0be86ecf5ff5bd135df1e2c47578367
Greg App <greg358@mail.ru>
2022-11-07 08:17:34 +0300

    feat: 06-build-page task: add copy/update directory (assets) functionality

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -29,86 +29,70 @@ async function getFileContentObjArray(folder, ext) {
   }
   return styleObjArr;
 }
-async function appendFileContent(trgtFilePath,dataArr) {
-  const fileContent=dataArr
-  .map((elem)=>Object.values(elem)[0])
-  .join('\n');
-  console.log('\nNew bundle.css file:\n-------------------\n',fileContent);
-  return fsProm.writeFile(trgtFilePath,fileContent);
+async function appendFileContent(trgtFilePath, dataArr) {
+  const fileContent = dataArr
+    .map((elem) => Object.values(elem)[0])
+    .join('\n');
+  //console.log('\nNew bundle.css file:\n-------------------\n', fileContent);
+  return fsProm.writeFile(trgtFilePath, fileContent);
 }
 /*-----Copy directory function---------START------*/
-async function copyDir (sourceFolder,targetFolder) {
-  return await new Promise(function(resolve,reject) {
-  let updFlag=[false,0,0];
-fsProm.readdir(sourceFolder, {
-    withFileTypes: true
-  })
-  .then((fileListObj) => {
-    updFlag[0]=true;
-    let fileList = [];
-    for (let file of fileListObj) {
-      if (file.isDirectory() !== true) {
-        fileList.push(file.name);
-      }
-    }
-    fsProm.mkdir(targetFolder, {
-        recursive: true
+const copyFrom = path.resolve(__dirname, 'assets');
+const copyTo = path.resolve(__dirname, 'project-dist', 'assets');
+
+async function copyDir(sourceFolder, targetFolder) {
+  return await new Promise(function (resolve, reject) {
+    let updFlag = [false, 0, 0];
+    fsProm.readdir(sourceFolder, {
+        withFileTypes: true
       })
-      .then(() => {
-        fsProm.readdir(targetFolder)
-          .then((oldTargetList) => {
-            if (oldTargetList.length > 0) {
-              for (let file of oldTargetList) {
-                if (!fileList.includes(file)) {
-                  updFlag[1]+=1;
-                  console.log('file in files-copy folder has to be removed: ', file);
-                  fs.rm(path.resolve(targetFolder, file), (err) => {
-                    if (err) {
-                      console.error(err.message);
+      .then((fileListObj) => {
+        updFlag[0] = true;
+        let fileList = [];
+        for (let file of fileListObj) {
+          if (file.isDirectory() !== true) {
+            fileList.push(file.name);
+          } else {
+            copyDir(path.resolve(sourceFolder, file.name), path.resolve(targetFolder, file.name));
+          }
+        }
+        fsProm.mkdir(targetFolder, {
+            recursive: true
+          })
+          .then(() => {
+            fsProm.readdir(targetFolder)
+              .then((oldTargetList) => {
+                for (let i = 0; i < fileList.length; i++) {
+                  updFlag[2] += 1;
+                  fsProm.copyFile(path.resolve(sourceFolder, fileList[i]), path.resolve(targetFolder, fileList[i]))
+                    .then(() => {
+                      console.log(`copy/update file complete from files to files-copy: ${fileList[i]}`);
+                      updFlag[2] -= 1;
+                      if (updFlag[1] === 0 && updFlag[2] === 0) {
+                        console.log('\ncopy/update directory done');
+                        resolve(true);
+                      }
+                    })
+                    .catch(err => {
+                      console.error(err);
                       reject(false);
                       throw err;
-                    }
-                    console.log(`REMOVE file complete from files-copy: ${file}`);
-                    updFlag[1]-=1;
-                    if(updFlag[1]===0&&updFlag[2]===0) {
-                      console.log('\ncopy/update directory done');
-                      resolve(true);
-                    }
-                  });
+                    });
                 }
-              }
-              console.log('\n');
-            }
-            for (let i = 0; i < fileList.length; i++) {
-              updFlag[2]+=1;
-              fsProm.copyFile(path.resolve(sourceFolder, fileList[i]), path.resolve(targetFolder, fileList[i]))
-                .then(() => {
-                  console.log(`copy/update file complete from files to files-copy: ${fileList[i]}`);
-                  updFlag[2]-=1;
-                  if(updFlag[1]===0&&updFlag[2]===0) {
-                    console.log('\ncopy/update directory done');
-                    resolve(true);
-                  }
-                })
-                .catch(err => {
-                  console.error(err);
-                  reject(false);
-                  throw err;
-                });
-            }
+              });
+          })
+          .catch(err => {
+            console.error(err.message);
+            console.log('AAAAAAAAA999999AAAAAAA');
+            reject(false);
+            throw err;
           });
       })
       .catch(err => {
         console.error(err.message);
-        reject(false);
         throw err;
       });
-  })
-  .catch(err => {
-    console.error(err.message);
-    throw err;
   });
-});
 }
 
 /*-----Copy directory function---------END------*/
@@ -117,20 +101,34 @@ async function buildPage() {
   let tempHtml = await fs.promises.readFile(path.resolve(__dirname, 'template.html'), 'utf-8');
   //console.log(tempHtml);
   let compHtmlArr = await getFileContentObjArray(path.resolve(__dirname, 'components'), 'html');
-   compHtmlArr= compHtmlArr.map((elem) => Object.entries(elem).flat());
+  compHtmlArr = compHtmlArr.map((elem) => Object.entries(elem).flat());
   //console.log(compHtmlArr);
   for (const comp of compHtmlArr) {
     if (tempHtml.match(`{{${comp[0]}}}`)) {
       tempHtml = tempHtml.replace(`{{${comp[0]}}}`, `${comp[1]}`);
     }
   }
-  await fsProm.mkdir(path.resolve(__dirname, 'project-dist'),{
+  await fsProm.mkdir(path.resolve(__dirname, 'project-dist'), {
     recursive: true
   });
-  await fsProm.writeFile(path.resolve(__dirname,'project-dist', 'index.html'), tempHtml);
-const styles=await getFileContentObjArray(path.resolve(__dirname, 'styles'), 'css');
-await appendFileContent(path.resolve(__dirname, 'project-dist','style.css'),styles);  
+  await fsProm.writeFile(path.resolve(__dirname, 'project-dist', 'index.html'), tempHtml);
+  const styles = await getFileContentObjArray(path.resolve(__dirname, 'styles'), 'css');
+  await appendFileContent(path.resolve(__dirname, 'project-dist', 'style.css'), styles);
+  const del= new Promise (function (resolve,reject) {
+    const date =new Date();
+    console.log('start',date.getMilliseconds());
+    fs.rm(path.resolve(__dirname,'project-dist','assets'),{recursive:true,force:true},()=>{
+      const dat =new Date();
+      console.log('end',dat.getMilliseconds());
+      resolve();});  
+  });
+  await del;
+   await copyDir(copyFrom, copyTo);
+  
+  //await copyDir(copyFrom, copyTo);
   return tempHtml;
 }
-buildPage();
+buildPage().then(res=>console.log('\n---------------------------\nBuild page done'));
 //.then(res=>console.log(res))
+
+console.log(path.dirname('index.js'));
